8a70e3e253ee9c73ce52d4b1ab0e58d0e3f69ce3
phisolucoes <solucoes.phi@gmail.com>
2022-07-30 06:42:04 -0300

    Feito a correção na rsponsividade

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var menu = document.getElementById("nav-menu");
+
+function opemMenu(){
+    menu.style.display = "flex";
+    menu.style.width = "100%";
+}
+
+function closeMenu(){
+    menu.style.display = "none";
+    menu.style.width = "0";
+}
